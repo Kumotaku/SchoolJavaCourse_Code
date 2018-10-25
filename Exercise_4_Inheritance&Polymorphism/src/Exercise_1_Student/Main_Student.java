c0486b76a3e577c3f341ebd247b852304bab76c5
Kumotaku <taku.ookami@outlook.com>
2018-10-25 13:27:46 +0800

    temp

---
+++ Exercise_4_Inheritance&Polymorphism/src/Exercise_1_Student/Main_Student.java
@@ -0,0 +1,19 @@
+/**
+ * 设计一个学生类Student，其属性有姓名、年龄和学位。
+ * 从Student类继承产生本科生类和研究生类，本科生增加专业属性，研究生增加研究方向属性。
+ * 每个类都有显示方法，用于输出属性信息。
+ */
+
+package Exercise_1_Student;
+
+public class Main_Student {
+    public static void main(String[] args){
+
+    }
+}
+
+class Student{
+    int age;
+    String name;
+    String degree;
+}
