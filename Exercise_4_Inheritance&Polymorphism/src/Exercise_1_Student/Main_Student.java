d294eaa551547351c43bf1e7429c90a3c00fcbfe
Kumotaku <taku.ookami@outlook.com>
2018-10-26 12:46:03 +0800

    Finish Exercise 1

--- Exercise_4_Inheritance&Polymorphism/src/Exercise_1_Student/Main_Student.java
+++ Exercise_4_Inheritance&Polymorphism/src/Exercise_1_Student/Main_Student.java
@@ -8,12 +8,66 @@ package Exercise_1_Student;
 
 public class Main_Student {
     public static void main(String[] args){
-
+        Undergraduate stu1 = new Undergraduate("Kumotaku", 15, "Undergraduate", "CS");
+        Graduate stu2 = new Graduate("Mobei", 17, "Graduate", "Alchemist");
+        stu1.Print();
+        stu2.Print();
     }
 }
 
-class Student{
-    int age;
-    String name;
-    String degree;
+//抽象类父类Student
+//抽象类只能被继承，不可被实例化
+abstract class Student{
+    private int age;
+    private String name;
+    private String degree;
+
+    //Student的构造函数
+    public Student(String name, int age, String degree){
+        this.age = age;
+        this.name = name;
+        this.degree = degree;
+    }
+
+    //打印类Student中的成员信息
+    public void Print(){
+        System.out.println("Name: " + name + "\nAge: " + age + "\nDegree: " + degree);
+    }
+}
+
+//继承父类Student的“大学生”子类Undergraduate
+class Undergraduate extends Student{
+    //包含一个私有成员：专业属性
+    private String MajorAttributes;
+
+    //构造父类及子类
+    public Undergraduate(String name, int age, String degree, String attribute) {
+        /*针对父类使用super(parameter1,parameter2,parameter3...)*/
+        super(name, age, degree);
+        this.MajorAttributes = attribute;
+    }
+
+    //打印父类及子类中的成员信息
+    public void Print(){
+        super.Print();
+        System.out.println("Major Attributes: " + MajorAttributes + "\n");
+    }
+}
+
+//继承父类Student的“研究生”子类Graduate
+class Graduate extends Student{
+    //包含一个私有成员：研究方向
+    private String ResearchDirection;
+
+    //构造父类及子类
+    public Graduate(String name, int age, String degree, String attribute) {
+        super(name, age, degree);
+        this.ResearchDirection = attribute;
+    }
+
+    //打印父类及子类中的成员信息
+    public void Print(){
+        super.Print();
+        System.out.println("Research Direction：" + ResearchDirection + "\n");
+    }
 }
