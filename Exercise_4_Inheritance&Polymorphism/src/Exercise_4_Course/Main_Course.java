2deefca26a20689d8b5bc09a160dd2670ec67cfb
Kumotaku <taku.ookami@outlook.com>
2018-10-29 14:11:24 +0800

    Finish Exercise 4

--- Exercise_4_Inheritance&Polymorphism/src/Exercise_4_Course/Main_Course.java
+++ Exercise_4_Inheritance&Polymorphism/src/Exercise_4_Course/Main_Course.java
@@ -5,38 +5,68 @@
 
 package Exercise_4_Course;
 
-public class Main_Course {
-    public static void main(){
+import java.util.ArrayList;
 
+public class Main_Course {
+    public static void main(String[] args){
+        Course course1 = new Course("Java Programming");
+        Course course2 = new Course("Computer Science");
+
+        course1.addStudents("Kumotaku");
+        course1.addStudents("Mobei");
+        course1.addStudents("Susars");
+
+        course2.addStudents("XTStarry");
+        course2.addStudents("Ainro");
+
+        System.out.println("Number of students in " + course1.getCourseName() +
+                " is " + course1.getNumberOfStudents() + " and they are: \n" + course1.students);
+        System.out.println("Number of students in " + course2.getCourseName() +
+                " is " + course2.getNumberOfStudents() + " and they are: \n" + course2.students);
+
+        course1.students.clear();
+        course2.students.clear();
+        System.out.println("清除course1，course2 中的学生后两门课程中人数分别为" +
+                course1.getNumberOfStudents() + "人，"+
+                course2.getNumberOfStudents() + "人。");
     }
 }
 
 class Course {
     private String courseName;
-    private String[] students = new String[100];
+    //改写数组的创建方法
+    ArrayList<String> students = new ArrayList<>();
     private int numberOfStudents;
 
     public Course(String courseName){
         this.courseName = courseName;
     }
 
-    public void addStudent(String student){
-        students[numberOfStudents] = student;
-        numberOfStudents ++;
+    //修改添加元素的方法：使用ArrayList方法修改掉数组方法
+    public void addStudents(String student){
+        students.add(student);
     }
 
-    public String[] getStudents(){
+    public ArrayList<String> getStudents() {
         return students;
     }
 
     public int getNumberOfStudents(){
-        return numberOfStudents;
+        return students.size();
     }
 
     public String getCourseName(){
         return courseName;
     }
 
-    public void dropStudent(String student){}
+    //JAVA 动态拓展数组 落实dropStudent 新增clear() 清除所有元素（学生）的功能
+    public void dropStudent(String student) {
+        // Left as an exercise in Exercise 9.9
+        students.remove(student);
+    }
+
+    public void clear(){
+        students.clear();
+    }
 }
 
